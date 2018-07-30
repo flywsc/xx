f948900debd7ce2a44d5ae650a564b9589096419
Administrator <Administrator@HX-015>
2018-07-30 11:26:31 +0800

    test

---
+++ wsc/src/com/wsc20180730/cell.java
@@ -0,0 +1,9 @@
+package com.wsc20180730;
+
+public class cell {
+   String colo;
+   String pingpai;
+   public void show() {
+	   System.out.println("这是一个"+pingpai+"的电池"+"  "+"它是"+colo+"颜色");
+   }
+}
---
+++ wsc/src/com/wsc20180730/testcell.java
@@ -0,0 +1,11 @@
+package com.wsc20180730;
+
+public class testcell {
+	public static void main(String[] args) {
+
+       cell ce=new cell();
+       ce.colo="°×É«";
+       ce.pingpai="ÄÏæÚ";
+       ce.show();
+	}
+}
