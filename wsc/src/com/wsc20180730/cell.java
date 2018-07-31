6aa8a6cfb61658d67f20d0a485e5a297f9335df8
Administrator <Administrator@HX-015>
2018-07-31 17:57:38 +0800

    2018.7.31日练习

--- wsc/src/com/wsc20180730/cell.java
+++ wsc/src/com/wsc20180730/cell.java
@@ -4,6 +4,6 @@ public class cell {
    String colo;
    String pingpai;
    public void show() {
-	   System.out.println("这是一个"+pingpai+"的电池"+"  "+"它是"+colo+"颜色");
+	   System.out.println("这个手机正在给"+pingpai+"的电池充电"+"  "+"它是"+colo);
    }
 }
