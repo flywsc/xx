e023cdc71e97ffab312088f7f3d9954b1167be7e
Administrator <Administrator@HX-015>
2018-08-01 19:58:16 +0800

    增加了银行的模拟

--- wsc/src/com/wsc20180731name/coustomer.java
+++ wsc/src/com/wsc20180731name/coustomer.java
@@ -19,5 +19,45 @@ public class coustomer {
 			System.out.println(names[i]);
 		}
 	}
-	
+	public void xiugai(String _old,String _new) {
+	    int index=-1;
+		for(int k=0;k<names.length&&names[k]!=null;k++) {
+			if(_old.endsWith(names[k])) {
+				index=k;
+				break;
+			}
+		}
+	    if(index!=-1) {
+	    	System.out.println("你输入的名字在第"+(index+1)+"找到");
+	    	names[index]=_new;
+	    	System.out.println("修改后的用户");
+	    	show();
+	    }else {
+	    	System.out.println("没有找到你输入的内容");
+	    }
+	}
+    public void shanchu(String _old1) {
+    	int index1=-1;//这一步就是找到要删除人名字的位置
+		for(int k=0;k<names.length&&names[k]!=null;k++) {
+			if(_old1.endsWith(names[k])) {
+				index1=k;
+				break;
+			}
+		}
+		if(index1!=-1) {//判断进入循环没有
+	    	System.out.println("你输入的名字在第"+(index1+1)+"找到");
+	    	names[index1]=null;
+	    	System.out.println("修改后的用户");
+	    	for(int i=0;i<names.length;i++) {
+				if(names[i]==null) {
+				continue;	
+				}
+				System.out.println(names[i]);
+			}
+	    }else {
+	    	System.out.println("没有找到你输入的内容");
+	    }
+	}
+    
 }
+
--- wsc/src/com/wsc20180731name/testcoustomer.java
+++ wsc/src/com/wsc20180731name/testcoustomer.java
@@ -9,7 +9,7 @@ public class testcoustomer {
 		coustomer cs = new coustomer();
 		Scanner input = new Scanner(System.in);
 		for (int i = 0; i < cs.names.length; i++) {
-			System.out.println("输入你将要增加的名字");
+			System.out.print("输入你将要增加的名字");
 			String name = input.next();
 			cs.add_name(name);
 			System.out.println("要继续加入名字吗        请选择 y_n");
@@ -23,7 +23,28 @@ public class testcoustomer {
 		}
 
 		cs.show();
-
+		
+		System.out.print("是否进行修改y_ _n");
+		if("y".equals(input.next())) {
+			System.out.print("请输入要修改的名字");
+			String _old=input.next();
+			System.out.print("请输入要修改的新名字");
+			String _new=input.next();
+			System.out.println();
+			cs.xiugai(_old,_new);
+		}else {
+			System.out.println("你选择不修改");
+		}
+		System.out.print("是否进行删除y_ _n");
+	  if("y".equals(input.next())) {
+		  System.out.print("请输入要删除的名字");
+			String _old1=input.next();
+			
+			System.out.println();
+			cs.shanchu(_old1);
+	  }else {
+		  System.out.println("你选择了不删除");
+	  }
 	}
 
 }
